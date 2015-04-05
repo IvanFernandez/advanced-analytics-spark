472c3bd6f03e8a0a4596d1a27ae66241b4d350f1
Ivan Fernandez <ivan.fernandez.perea@gmail.com>
2015-04-05 15:47:09 +0200

    Updated DecisionTreeTest class

--- src/main/scala/DecisionTreeTest.scala
+++ src/main/scala/DecisionTreeTest.scala
@@ -13,12 +13,37 @@ import org.apache.spark.mllib.util.MLUtils
 object DecisionTreeTest {
   def main(args: Array[String]) {
 
-    val conf = new SparkConf().setMaster("local[2]").setAppName("StreamingKMeansTest")
+    val conf = new SparkConf().setMaster("local[2]").setAppName("DecisionTreeTest")
+    val sc = new SparkContext(conf)
+    // Load and parse the data file.
+    val data = MLUtils.loadLibSVMFile(sc, "/home/centos/data/decisionTrees")
+    // Split the data into training and test sets (30% held out for testing)
+    val splits = data.randomSplit(Array(0.7, 0.3))
+    val (trainingData, testData) = (splits(0), splits(1))
 
+    // Train a DecisionTree model.
+    //  Empty categoricalFeaturesInfo indicates all features are continuous.
+    val numClasses = 2
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val impurity = "gini"
+    val maxDepth = 5
+    val maxBins = 32
 
+    val model = DecisionTree.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+      impurity, maxDepth, maxBins)
 
+    // Evaluate model on test instances and compute test error
+    val labelAndPreds = testData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+    val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
+    println("Test Error = " + testErr)
+    println("Learned classification tree model:\n" + model.toDebugString)
 
+    // Save and load model
+    model.save(sc, "myModelPath")
+    val sameModel = DecisionTreeModel.load(sc, "myModelPath")
   }
 }
-
 
